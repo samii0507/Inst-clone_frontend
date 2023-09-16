8527c19feec338ce5d9ce0b8108a49fd1aaefed2
Samitha <samitha.nillupul1440@gmail.com>
2023-09-16 12:54:58 +0530

    basic side bar created

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import {IoReorderThreeOutline} from "react-icons/io5";
+import { mainu } from './SideBarConfig';
 
 const SideBar = () => {
   return (
-    <div>
-        <div>
+    <div className='sticky top-0 h-[100vh]'>
+        <div className='flex flex-col justify-between h-full'>
             <div>
-            <img className='w-40 ' src='https://i.imgur.com/zqpwkLQ.png' alt='instagram' />
+             <div className='pt-10'>
+               <img className='w-40 ' src='https://i.imgur.com/zqpwkLQ.png' alt='instagram' />
+            </div>
+            <div className='mt-10'>
+               {mainu.map((item) =>  <div className='flex items-center mb-5 cursor-pointer text-lg'>
+                         {item.icon}
+                           <p>{item.title}</p>
+                           
+                       </div>)}
+            </div>
+            
+            
+
             </div>
         </div>
         <div className='flex items-center cursor-pointer pb-10'>
